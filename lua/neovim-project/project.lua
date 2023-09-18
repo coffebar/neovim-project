7875e7d15fed94e258d8d9cb0c8cf4f6b96d4882
coffeebar <i8ehkvien@mozmail.com>
2023-09-18 11:49:43 +0300

    fix: DirChanged was not fired when CD in autocmd

--- lua/neovim-project/project.lua
+++ lua/neovim-project/project.lua
@@ -4,11 +4,12 @@ local history = require("neovim-project.utils.history")
 local manager = require("session_manager")
 local path = require("neovim-project.utils.path")
 
-M.switing_project_to = nil
+M.save_project_waiting = false
 
 M.setup_autocmds = function()
   local augroup = vim.api.nvim_create_augroup("neovim-project", { clear = true })
 
+  -- save history to file when exit nvim
   vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
     pattern = "*",
     group = augroup,
@@ -16,6 +17,7 @@ M.setup_autocmds = function()
       history.write_projects_to_history()
     end,
   })
+  -- add project to history when open nvim in project's directory
   vim.api.nvim_create_autocmd({ "User" }, {
     pattern = "SessionLoadPost",
     group = augroup,
@@ -26,15 +28,12 @@ M.setup_autocmds = function()
       end
     end,
   })
+  -- switch project after save previous session
   vim.api.nvim_create_autocmd({ "User" }, {
     pattern = "SessionSavePost",
     group = augroup,
     callback = function()
-      if M.switing_project_to ~= nil then
-        local dir = M.switing_project_to
-        M.switing_project_to = nil
-        M.load_session(dir)
-      end
+      M.save_project_waiting = true
     end,
   })
 end
@@ -45,10 +44,16 @@ end
 
 function M.switch_after_save_session(dir)
   -- Switch project after saving current session
-  M.switing_project_to = dir
+  --
   -- save current session
   -- before switch project
+  M.save_project_waiting = true
   manager.save_current_session()
+  -- wait for SessionSavePost autocmd or timeout 2 sec
+  vim.wait(2000, function()
+    return not M.switing_project
+  end, 1)
+  M.load_session(dir)
 end
 
 function M.load_session(dir)
