e7868b38f402be94e859d479002df1418bc1e954
Saltaformajo <i8ehkvien@mozmail.com>
2023-12-12 09:23:56 +0200

    fix: use ps to get command line (#17)

--- lua/neovim-project/utils/cmd.lua
+++ lua/neovim-project/utils/cmd.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.check_open_cmd = function(cmd)
-  local open_cmd = vim.fn.split(vim.fn.readfile("/proc/self/cmdline")[1], "\n", 1)
+  local open_cmd = vim.fn.split(vim.fn.systemlist("ps -o command -p " .. vim.fn.getpid())[2], " ", 1)
   local found = false
 
   for _, arg in ipairs(open_cmd) do
